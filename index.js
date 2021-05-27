c53fd30d7e2c900fd0b79bc6c03c50e1ee884aa2
Luan de Almeida <luanbianchine1@gmail.com>
2021-05-27 13:28:09 -0300

    primeiro commit
    Base de qualquer aplicação node, nao foi feito nada ainda, apenas a arquitetura e a organização do projeto

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const connection = require('./database/database');
+
+//configurando a view engine
+app.set("view engine", 'ejs');
+
+//arquivos estaticos
+app.use(express.static("public"));
+
+//Body-parser
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+//Conectando com o banco de dados
+connection
+    .authenticate()
+    .then(() => {
+        console.log("conexão com o banco de dados feita com sucesso");
+    }).catch((error) => {
+        console.log(error);
+    })
 
 //rotas
 app.get("/", (req, res) => {
-    res.send("Bem vindo ao meu site");
+    res.render('index');
 });
 
+//servidor
 app.listen(5500, () => {
     console.log("O servidor está rodando");
 })
