fa6ea85aa78bb544380e8a1a4bd8b03ed871b85a
Luan de Almeida <luanbianchine1@gmail.com>
2021-05-31 13:37:59 -0300

    sistema de login (autenticação)

--- user/usersController.js
+++ user/usersController.js
@@ -94,4 +94,31 @@ router.post('/users/save', (req,res) => {
     }
 });
 
+router.get("/login", (req, res) => {
+    res.render("admin/users/login");
+});
+
+router.post("/authenticate", (req, res) => {
+    var email = req.body.email;
+    var password = req.body.password;
+    User.findOne({
+        where: {email: email}
+    }).then(user => {
+        if(user != undefined){ //se  o e-mail existe
+            //validar senha
+            var correct = bcrypt.compareSync(password, user.password); //comparando a senha no formulario com o banco de dados
+            if(correct){
+                req.session.user = {
+                    id: user.id,
+                    email: user.email
+                }
+                res.json(req.session.user)
+            } else{ 
+                res.redirect("/login");
+            }
+        } else{
+            res.redirect("/login");
+        }
+    })
+});
 module.exports = router;
