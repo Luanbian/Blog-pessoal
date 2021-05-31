19e2c308f9befbc0efcd500b0b1c407348ae1ed3
Luan de Almeida <luanbianchine1@gmail.com>
2021-05-31 11:10:53 -0300

    listagem e edições de cadastros

--- user/usersController.js
+++ user/usersController.js
@@ -4,7 +4,9 @@ const User = require('./User');
 const bcrypt = require("bcryptjs");
 
 router.get('/admin/users', (req, res) => {
-    res.send("lista de usuarios");
+    User.findAll().then(users => {
+        res.render("admin/users/index", {users: users});
+    });
 });
 
 router.get("/admin/users/create", (req,res) => {
@@ -34,6 +36,62 @@ router.post("/users/create", (req,res) => {
             res.redirect("/admin/users/create");
         }
     });
+});
+
+router.post("/users/delete", (req, res) => {
+    var id = req.body.id;
+    if(id != undefined){
+        if(!isNaN(id)){
+            User.destroy({
+                where: {id: id}
+            }).then(() => {
+                res.redirect("/admin/user");
+            })
+        } else{ //se nao for um numero
+            res.redirect('/admin/users')
+        }
+    } else{ //se for nulo
+        res.redirect('/admin/users')
+    }
+})
+
+router.get("/admin/users/edit/:id", (req, res) => {
+    var id = req.params.id;
+    if(isNaN(id)){
+        res.redirect("/admin/users");
+    }
+    User.findByPk(id).then(user => {
+        if(user != undefined){
+            res.render("admin/users/edit", {user: user});
+        }else {
+            res.redirect("/admin/users");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/users");
+    })
+});
+
+router.post("/users/update" ,(req, res) => {
+    var id = req.body.id;
+    var email = req.body.email;
+    User.update({email: email}, {
+        where: {id: id}
+    }).then(() =>{
+        res.redirect("/admin/users");
+    })
+});
+
+router.post('/users/save', (req,res) => {
+    var email = req.body.email;
+    if(email != undefined){
+        User.create({
+            email: email
+        }).then(()=> {
+            res.redirect("/admin/users");
+        })
+    } else{
+        res.redirect('admin/users/create')
+    }
 });
 
 module.exports = router;
